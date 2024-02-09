bd021fea7d749470817a61318cb0d8f2fb43ae7b
-g <-g>
2024-02-09 21:51:42 +0500

    move code

---
+++ src/data/products.js
@@ -0,0 +1,123 @@
+export const products = [
+  {
+    id: 1,
+    name: "The Unicorn",
+    description: "They exist!",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["unicorn.jpg", "unicorn1.jpg", "unicorn2.jpg"],
+  },
+  {
+    id: 2,
+    name: "The Fashionista",
+    description: "You know he's got his own personal stylist.",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["fashionista.jpg", "fashionista1.jpg", "fashionista2.jpg"],
+  },
+  {
+    id: 3,
+    name: "The Drooler",
+    description: "Look at that crazy and cute face!",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["drooler.jpg", "drooler1.jpg", "drooler2.jpg"],
+  },
+  {
+    id: 4,
+    name: "The Player",
+    description: "Catch this sticker today!",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["player.jpg", "player1.jpg", "player2.jpg"],
+  },
+  {
+    id: 5,
+    name: "Scaredy Dog",
+    description: "No surprise courage and him are best friends.",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["Scaredy-Dog.jpg", "Scaredy-Dog1.jpg", "Scaredy-Dog2.jpg"],
+  },
+  {
+    id: 6,
+    name: "The Pee-er",
+    description: "Public enemy No. 1",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["Pee-er.jpg", "Pee-er1.jpg", "Pee-er2.jpg"],
+  },
+  {
+    id: 7,
+    name: "The Brawl",
+    description: "Let's just say a lot of bones were broken...and chewed on.",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["Brawl.jpg", "Brawl1.jpg", "Brawl2.jpg"],
+  },
+  {
+    id: 8,
+    name: "Tip Toe Pub",
+    description: "So sneaky...  ",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["Tip-Toe-Pub.jpg", "Tip-Toe-Pub1.jpg", "Tip-Toe-Pub2.jpg"],
+  },
+  {
+    id: 9,
+    name: "Dog Bath",
+    description: "Scrub em clean!",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["Dog-Bath.jpg", "Dog-Bath1.jpg", "Dog-Bath2.jpg"],
+  },
+  {
+    id: 10,
+    name: "Tongue Wagger",
+    description: "Tis a slobby one",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["Wagger.jpg", "Wagger1.jpg", "Wagger2.jpg"],
+  },
+  {
+    id: 11,
+    name: "Angry Dog",
+    description: "Isn't he cute when he's angry?",
+    sizes: [
+      { label: "3 x 3", price: 9.99 },
+      { label: "4 x 4", price: 12.99 },
+      { label: "5.5 x 5.5", price: 15.99 },
+    ],
+    images: ["Angry-Dog.jpg", "Angry-Dog1.jpg", "Angry-Dog2.jpg"],
+  },
+];
